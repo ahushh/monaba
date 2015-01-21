268bfb525b0856e7e8740d66bff10a86c7633f4b
Ahushh <ahushh@gmail.com>
2015-01-21 05:16:46 +0300

    Display files from news

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -15,8 +15,11 @@ getHomeR = do
   boardCategories <- getConfig configBoardCategories
   newsBoard       <- getConfig configNewsBoard
   showNews        <- getConfig configShowNews
-  latestNews      <- runDB $ selectList [PostBoard ==. newsBoard, PostParent ==. 0, PostDeleted ==. False] [Desc PostLocalId, LimitTo showNews]
-  timeZone        <- getTimeZone
+  let selectFiles p = runDB $ selectList [AttachedfileParentId ==. entityKey p] []
+      selectNews    = selectList [PostBoard ==. newsBoard, PostParent ==. 0, PostDeleted ==. False]
+                                 [Desc PostLocalId, LimitTo showNews]
+  newsAndFiles <- runDB selectNews >>= mapM (\p -> selectFiles p >>= (\files -> return (p, files)))
+  timeZone     <- getTimeZone
   defaultLayout $ do
     setTitle $ toHtml nameOfTheBoard
     $(widgetFile "homepage")

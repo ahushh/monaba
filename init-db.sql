d44c82c7c404696612216c03941c2e8e5297568d
Ahushh <ahushh@gmail.com>
2015-12-25 10:57:20 +0300

    fix init-db.sql

--- init-db.sql
+++ init-db.sql
@@ -1,4 +1,4 @@
 INSERT INTO "storage" VALUES (1,0);
-INSERT INTO "config" VALUES (1,7,30,'["s"]','news',2,10,15,'nothing to see here','<img id="main-image" src="static/home.jpg"',3,36000);
+INSERT INTO "config" VALUES (1,7,30,'["s"]','news',2,10,15,'nothing to see here','<img id="main-image" src="static/home.jpg">',3,36000);
 INSERT INTO "group" VALUES (1,'Admin','["sManageThreadP","sManageBoardP","sManageUsersP","sManageConfigP","sDeletePostsP","sManagePanelP","sManageBanP","sEditPostsP","sAdditionalMarkupP","sViewModlogP",\"sShadowEditP\"]');
 INSERT INTO "user" VALUES (1,'admin','Admin','de41b7fb99201d8334c23c014db35ecd92df81bc','1');

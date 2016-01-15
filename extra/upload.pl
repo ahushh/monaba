170f67904bdea688195d9a099b1a5a3e096b6fc6
Ahushh <ahushh@gmail.com>
2016-01-15 06:58:11 +0300

    Update upload script

--- extra/upload.pl
+++ extra/upload.pl
@@ -1,24 +1,25 @@
 #!/usr/bin/perl
 use v5.12;
 ####################################################
-my $user = "user";
-my $host = "host.ru";
-my $path = "/home/user/Monaba/dist/build/Monaba/";
-my $key  = "$ENV{HOME}/notes/passwords/ssh_key";
+my $user = "ahushh";
+my $host = "haibane.ru";
+my $path = "/home/ahushh/haibane/Monaba/dist/build/Monaba/";
+my $key  = "$ENV{HOME}/.ssh/id_rsa";
 #systemd
-#my $start_cmd = "sudo systemctl start monaba.service";
-#my $stop_cmd = "sudo systemctl stop monaba.service";
+my $start_cmd = "systemctl --user start monaba";
+my $stop_cmd = "systemctl --user stop monaba";
 #openrc
-my $start_cmd = "sudo /etc/init.d/monaba start";
-my $stop_cmd = "sudo /etc/init.d/monaba stop";
+#my $start_cmd = "sudo /etc/init.d/monaba start";
+#my $stop_cmd = "sudo /etc/init.d/monaba stop";
 ####################################################
-die "usage: ./upload.pl [version] [build|pack|upload|restart|all]" unless defined $ARGV[0] && defined $ARGV[1];
+die "usage: ./upload.pl [version] [build|pack|upload|restart|clean|all]" unless defined $ARGV[0] && defined $ARGV[1];
 my $v = $ARGV[0];
 my $_ = $ARGV[1];
-my $build  = /build|all/    ? "cabal clean&&yesod configure&&yesod build" : 'echo "skip building"';
+my $build  = /build|all/    ? "cabal clean && yesod build" : 'echo "skip building"';
 my $pack   = /pack|all/     ? "cd dist/build/Monaba && strip Monaba && cp Monaba Monaba-$v && apack Monaba-$v.7z Monaba-$v" : 'echo "skip packing"';
 my $upload = (/upload/      ? "cd dist/build/Monaba &&" : "") .
-             (/upload|all/  ? "scp -i $key Monaba-$v.7z ${user}\@${host}:${path}" : 'echo "skip uploading"');
+             (/upload|all/  ? "scp ". ($key ? "-i $key" : "") ." Monaba-$v.7z ${user}\@${host}:${path}" : 'echo "skip uploading"');
 my $restart= (/restart/     ? "cd dist/build/Monaba &&" : "") .
-             (/restart|all/ ? "ssh -i $key -t ${user}\@${host} 'cd $path && aunpack Monaba-$v.7z && $stop_cmd && cp Monaba-$v Monaba && $start_cmd && echo done'" : 'echo "skip restarting"');
-system "$build && $pack && $upload && $restart";
+             (/restart|all/ ? "ssh ". ($key ? "-i $key" : "") ." -t ${user}\@${host} 'cd $path && aunpack Monaba-$v.7z && $stop_cmd && cp Monaba-$v Monaba && $start_cmd && echo done'" : 'echo "skip restarting"');
+my $clean =  (/clean|all/   ? "ssh -i $key -t ${user}\@${host} 'cd $path && rm Monaba-$v.7z Monaba-$v && echo done cleaning'" : 'echo "skip cleaning"');
+system "$build && $pack && $upload && $restart && $clean";
